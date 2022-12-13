17e0d2261e0449fe84b20ab09d2c9cf8e45573a7
anujamin01 <59538544+anujamin01@users.noreply.github.com>
2022-12-13 13:31:34 -0600

    Update msg.h

--- msg.h
+++ msg.h
@@ -16,18 +16,18 @@ typedef struct{
     int pinum;
     char name[4096];
     int inum;
-    MFS_Stat_t *m;
+    // MFS_Stat_t *m; potentially remove
     char buffer[4096];
     int offset;
     int nbytes;
     int type;
 } msg_t;
-
+// move buffer outside and increase size then concatenate the struct msg_t to the buffer in the mfs write or whatever
 typedef struct{
     int inode;
     int type;
     int size;
     char buffer[4096];
     int returnCode;
-    MFS_Stat_t *m;
+    MFS_Stat_t m;
 } s_msg_t;
